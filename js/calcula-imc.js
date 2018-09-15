fa51d19e14d3026d99be104794f06359a4b6b038
Angelo Lima <angelolimadias@gmail.com>
2018-09-15 07:16:39 -0300

    aula 5

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -3,45 +3,62 @@ var pacientes = document.querySelectorAll(".paciente");
 
 // Loop que percorre os pacientes da tabela e calcula o IMC
 for (var i = 0; i < pacientes.length; i = i + 1) {
-	// Variavel referente ao inidice do paciente
-	var paciente = pacientes[i];
 
-	// Variaveis elemento td
-	var tdPeso = paciente.querySelector(".info-peso");
-	var tdAltura = paciente.querySelector(".info-altura");
-	var tdImc = paciente.querySelector(".info-imc");
+    //variavel referente ao indice do paciente
+    var paciente = pacientes[i];
 
-	// Variaveis dados da tabela
-	var peso = tdPeso.textContent;
-	var altura = tdAltura.textContent;
-	var imcpaciente = tdImc.textContent;
+    
+    // variaveis elemento td
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
 
-	var alturaEhValida = true;
-	var pesoEhValido = true;
+    //Variaveis dados da tabela
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
-	// Validação de Altura e Peso
-	if (peso >= 200 || peso <= 0) {
-		pesoEhValido = false;
-		tdImc.textContent = "Peso Inválido";
-		//pintando tr com peso invalido
-		paciente.classList.add("paciente-invalido");
-	}
-	if (altura >= 3.00 || altura <= 0) {
-		alturaEhValida = false;
-		tdImc.textContent = "Altura Inválida"
-		//pintando tr com altura invalida
-		paciente.classList.add("paciente-invalido");
-	}
+    // Variaveis de validação
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValido = validaAltura(altura);
 
-	// Calculo IMC
-	if (pesoEhValido && alturaEhValida) {
-		tdImc.textContent = calculaImc(peso, altura);
-	}
+    // Validação altura e peso
+    if (!pesoEhValido) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if (!alturaEhValido) {
+        alturaEhValido = false;
+        tdImc.textContent = "Altura Inválido";
+        paciente.classList.add('paciente-invalido');
+    }
+
+    // Calculo IMC
+    if (pesoEhValido && alturaEhValido) {
+        tdImc.textContent = calculaImc(peso, altura);
+    }
 }
+
+function validaPeso(peso){
+    if (peso <=200 && peso >= 0 ){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if (altura <= 3.00 && altura >= 0){
+        return true;
+    } else{
+        return false;
+    }
+}
+
 
 function calculaImc(peso, altura) {
-	var imc = 0;
-	imc = peso / (altura * altura);
-	return imc.toFixed(2);
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
 }
-
